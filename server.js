4dc386c04a79791d6ed603aae9005b09be9c459d
emaron01 <maroneyerik@hotmail.com>
2026-01-19 16:58:45 -0500

    added run_count increment logic and status callback

--- server.js
+++ server.js
@@ -323,4 +323,5 @@ app.post("/agent", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Agent live on port ${PORT}`));
